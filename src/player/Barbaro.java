f0dbf645e3a7ce4213964fd88396c1a19ff71565
Crys-bit7 <crystiankr.rox@gmail.com>
2026-06-22 19:47:26 -0300

    Oiii

---
+++ src/player/Barbaro.java
@@ -0,0 +1,34 @@
+package player;
+
+public class Barbaro extends Personagem{
+    public Barbaro(){
+        super();
+        setAtaque(15);
+        setDefesa(11);
+
+
+    }
+    @Override
+    public void atacar(Personagem alvo){
+
+        if(alvo.isDefendendo()){
+            getAtaque();
+        }
+
+        alvo.setVida(alvo.getVida() - this.getAtaque());
+        if (alvo.getVida() < 0) {
+            alvo.setVida(0);
+        }
+        System.out.println("Você causou: " + getAtaque() + " de dano, o inimigo tem: " + alvo.getVida() + " pontos de vida restantes.");
+    }
+
+}
+
+
+
+
+
+
+
+
+
---
+++ src/player/Paladino.java
@@ -0,0 +1,44 @@
+package player;
+
+public class Paladino extends Personagem {
+    private int mana = 10;
+    private int danoMagico = 12;
+
+    public Paladino() {
+        super();
+        setAtaque(10);
+        setDefesa(13);
+    }
+
+    public int getDanoMagico() {
+        return danoMagico;
+    }
+
+    public void setDanoMagico(int danoMagico) {
+        this.danoMagico = danoMagico;
+    }
+
+
+    public void atacarMagia(Personagem alvo) {
+        alvo.setVida(alvo.getVida() - danoMagico);
+        if (alvo.getVida() < 0) {
+            alvo.setVida(0);
+        }
+        System.out.println("Você causou: " + danoMagico + " de dano mágico, o inimigo tem: " + alvo.getVida() + " pontos de vida restantes.");
+    }
+
+    @Override
+    public void atacar(Personagem alvo) {
+
+        if (alvo.isDefendendo()) {
+            getAtaque();
+        }
+
+        alvo.setVida(alvo.getVida() - this.getAtaque());
+        if (alvo.getVida() < 0) {
+            alvo.setVida(0);
+        }
+        System.out.println("Você causou: " + getAtaque() + " de dano, o inimigo tem: " + alvo.getVida() + " pontos de vida restantes.");
+    }
+
+}
